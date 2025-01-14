eea8974e56da12170b8e7f259bd859e51c509b1f
Faisal-Hussein <fh069017@ohio.edu>
2025-01-14 16:51:09 -0500

    Added more styling

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,13 +22,20 @@ function Popular() {
     <div>
           <Wrapper>
             <h3>Popular</h3>
-            <Splide>
+            <Splide options={{
+              perPage: 4,
+              arrows: false,
+              pagination: false,
+              drag: 'free',
+              gap: '5rem'
+            }}>
               {popular.map((recipe)=> {
                 return(
                   <SplideSlide>
                     <Card>
                       <p>{recipe.title}</p>
                       <img src={recipe.image} alt={recipe.title} />
+                      <Gradient />
                     </Card>
                   </SplideSlide>
                 );
@@ -45,10 +52,39 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+    p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display-flex;
+    justify-content: center;
+    align-items: center;
+    }
 `;
 
-export default Popular
+const Gradient = styled.div`
+z-index: 3;
+position: absolute;
+width: 100%;
+height: 100%;
+background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+export default Popular;
