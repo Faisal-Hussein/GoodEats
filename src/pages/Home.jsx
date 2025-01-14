87321d3c6912866988ddb45e58ededd066e711dc
Faisal-Hussein <fh069017@ohio.edu>
2025-01-14 14:03:40 -0500

    Pages folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default home
