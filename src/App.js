1da45e327a61277fdfaae4c5c8db991febdf75a2
Faisal-Hussein <fh069017@ohio.edu>
2025-02-04 14:12:21 -0500

    Modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Pages from "./pages/Pages";
 import Section from "./components/Section";
 import { BrowserRouter } from 'react-router-dom'
+import Search from "./components/Search";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Section />
         <Pages />
       </BrowserRouter>
