e1805ac1655a78938d09a6317f97c149c1eebd4d
Faisal-Hussein <fh069017@ohio.edu>
2025-02-04 13:05:58 -0500

    Added different pages for different sections

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Home from './Home';
 import Cuisine from './Cuisine';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cuisine" element={<Cuisine />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
     </Routes>
   );
 }
